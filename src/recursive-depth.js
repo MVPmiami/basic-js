92873c0b5c3c9220bb080163dfdbf69c03f95773
Aliaksei Siankevich <andfly@tut.by>
2020-10-31 19:00:03 +0300

    add: add 3th tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+  calculateDepth(arr) {
+    let countArr = 1;
+    let level = 1;
+      for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i]) ){
+          countArr = 1 + this.calculateDepth(arr[i]);
+        }else{
+          continue;
+        }
+        this.calculateDepth(arr[i]);
+      }
+      if(countArr > level){
+        level = countArr;
+      }
+      countArr = 1;
+      return level;
+    }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  result: '',
+  isInt : true,
+  chain: [] ,
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    //console.log(this.chain.length);
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(this.chain.length === 0){
+      this.result = '';
+    }
+    if(arguments.length === 0){
+      this.chain.push(' ');
+    }else{
+      this.chain.push(`( ${value} )~~`);
+      //console.log(this.chain);
+      return this;
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    isInteger(position)
+    if(position <= 0 || position > this.chain.length || typeof position !== 'number' || this.isInt === false){
+      this.chain = [];
+      this.result = '';
+      throw Error('THROWN');
+    }else{
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    for(let i = 0; i < this.chain.length; i++){
+        this.result += this.chain[i];
+    }
+    this.result = this.result.substring(0,this.result.length - 2);
+    //console.log(this.result);
+    this.chain = [];
+    return this.result;
   }
+};
+
+function isInteger(position) {
+  //console.log(chainMaker.isInt = (position ^ 0) === position)
+  return chainMaker.isInt = (position ^ 0) === position;
 };
 
 module.exports = chainMaker;
