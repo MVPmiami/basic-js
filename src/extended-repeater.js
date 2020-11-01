a56e080247f842991774902363e404f2ee45eb79
Aliaksei Siankevich <andfly@tut.by>
2020-11-01 19:17:04 +0300

    add 2tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,82 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let repeatStr = str;
+  if(repeatStr == null){
+    repeatStr = 'null'
+  }else{
+    repeatStr = repeatStr.toString();  
+  }
+  let repeatTimes = 0; //устанавливает число повторений
+  let separator = '+';//это строка, разделяющая повторения
+  let addition ;//это дополнительная строка, которая будет добавлена после каждого повторения
+  let additionRepeatTimes = 0; // устанавливает число повторений addition
+  let additionSeparator = '|'; // это строка, разделяющая повторения addition
+  
+  let arrString = [];
+  let result = '';
+  // получаем все опции
+  if(options.repeatTimes == undefined){
+    repeatTimes = 0;
+  }else{
+    repeatTimes = options.repeatTimes
+  }
+  
+  if(options.separator == undefined){
+    separator = '+';
+  }else{
+    separator = options.separator;
+  }
+  
+  if(options.addition === undefined){
+    addition = '';
+  }else if(options.addition == null){
+    addition = 'null';
+  }else{
+      addition = options.addition.toString();
+  }
+  
+  if(options.additionRepeatTimes == undefined){
+    additionRepeatTimes = 0;
+  }else{
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  
+  if(options.additionSeparator == undefined){
+    additionSeparator = '|';
+  }else{
+    additionSeparator = options.additionSeparator;
+  }
+  
+  if(options.repeatTimes == undefined && options.additionRepeatTimes == undefined){
+    arrString.push(repeatStr);
+    arrString.push(addition);
+  }else{
+    for(let i = 0; i < repeatTimes; i ++){
+      arrString.push(repeatStr);
+      for(let n = 0; n < additionRepeatTimes; n++){
+        if( n === additionRepeatTimes - 1){
+          arrString.push(addition);
+          continue;
+        }else{
+          arrString.push(addition);
+          arrString.push(additionSeparator);  
+        }
+      }
+      if(i === repeatTimes - 1){
+        continue;
+      }else{
+        arrString.push(separator);
+      }
+    }
+  }
+  
+  arrString.forEach( function(item) {
+    result += item;
+  });
+  
+  //console.log(result);
+  return result;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,186 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    let numSymbols = {
+      a: 0,
+      b: 1,
+      c: 2,
+      d: 3,
+      e: 4,
+      f: 5,
+      g: 6,
+      h: 7,
+      i: 8,
+      j: 9,
+      k: 10,
+      l: 11,
+      m: 12,
+      n: 13,
+      o: 14,
+      p: 15,
+      q: 16,
+      r: 17,
+      s: 18,
+      t: 19,
+      u: 20,
+      v: 21,
+      w: 22,
+      x: 23,
+      y: 24,
+      z: 25,
+    }
+    let strLowerCase = str.toLowerCase();
+    let keyLowerCase = key.toLowerCase();
+    let numStrArr = [];
+    let numKeyArr = [];
+    let resultArr = [];
+    let sum ;
+    let difference;
+    let resultCipher = [];
+    let result = '';
+    // присваиваем числа каждой букве строки
+    for(let i = 0; i < strLowerCase.length; i++){
+      for(key in numSymbols){
+        if(key === strLowerCase[i]){
+          numStrArr.push(numSymbols[key]);
+        }
+      }
+    }
+    // присваиваем числа каждой букве ключа
+    for(let i = 0; i < keyLowerCase.length; i++){
+      for(key in numSymbols){
+        if(key === keyLowerCase[i]){
+          numKeyArr.push(numSymbols[key]);
+        }
+      }
+    }
+    // сравниваем длину ключа со строкой
+    if(numStrArr.length >= numKeyArr.length){
+      difference = numStrArr.length - numKeyArr.length;
+      for(let i = 0; i < difference; i++){
+        numKeyArr.push(numKeyArr[i]);
+      }
+    }else{
+      difference = numKeyArr.length - numStrArr.length;
+      for(let i = 0; i < difference; i++){
+        numKeyArr.pop()
+      }
+    }
+    
+    // складываем числа массивов строки и ключа
+    for(let i = 0; i < numStrArr.length; i++){
+      sum = numStrArr[i] + numKeyArr[i];
+      if(sum >= 26){
+        sum = sum - 26;
+      }
+      resultArr.push(sum);
+    }
+    // получаем шифрованную строку
+    for(let i = 0; i < resultArr.length; i++){
+      for(key in numSymbols){
+        if(numSymbols[key] === resultArr[i]){
+          resultCipher.push(key);
+        }
+      }
+    }
+    resultCipher.forEach(function(item){
+      result += item;
+    });
+
+    console.log(result.toUpperCase());
+    return result.toUpperCase();
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    let numSymbols = {
+      a: 0,
+      b: 1,
+      c: 2,
+      d: 3,
+      e: 4,
+      f: 5,
+      g: 6,
+      h: 7,
+      i: 8,
+      j: 9,
+      k: 10,
+      l: 11,
+      m: 12,
+      n: 13,
+      o: 14,
+      p: 15,
+      q: 16,
+      r: 17,
+      s: 18,
+      t: 19,
+      u: 20,
+      v: 21,
+      w: 22,
+      x: 23,
+      y: 24,
+      z: 25,
+    }
+    let strLowerCase = str.toLowerCase();
+    let keyLowerCase = key.toLowerCase();
+    let numStrArr = [];
+    let numKeyArr = [];
+    let resultArr = [];
+    let sum ;
+    let difference;
+    let resultCipher = [];
+    let result = '';
+    // присваиваем числа каждой букве строки
+    for(let i = 0; i < strLowerCase.length; i++){
+      for(key in numSymbols){
+        if(key === strLowerCase[i]){
+          numStrArr.push(numSymbols[key]);
+        }
+      }
+    }
+    // присваиваем числа каждой букве ключа
+    for(let i = 0; i < keyLowerCase.length; i++){
+      for(key in numSymbols){
+        if(key === keyLowerCase[i]){
+          numKeyArr.push(numSymbols[key]);
+        }
+      }
+    }
+    // сравниваем длину ключа со строкой
+    if(numStrArr.length >= numKeyArr.length){
+      difference = numStrArr.length - numKeyArr.length;
+      for(let i = 0; i < difference; i++){
+        numKeyArr.push(numKeyArr[i]);
+      }
+    }else{
+      difference = numKeyArr.length - numStrArr.length;
+      for(let i = 0; i < difference; i++){
+        numKeyArr.pop()
+      }
+    }
+    
+    // складываем числа массивов строки и ключа
+    for(let i = 0; i < numStrArr.length; i++){
+      sum = numStrArr[i] - numKeyArr[i];
+      if(sum < 0){
+        sum = 26 + sum;
+      }
+      resultArr.push(sum);
+    }
+    // получаем шифрованную строку
+    for(let i = 0; i < resultArr.length; i++){
+      for(key in numSymbols){
+        if(numSymbols[key] === resultArr[i]){
+          resultCipher.push(key);
+        }
+      }
+    }
+    resultCipher.forEach(function(item){
+      result += item;
+    });
+
+    console.log(result.toUpperCase());
+    return result.toUpperCase();
   }
 }
 
